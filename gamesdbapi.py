52453c63cdc25a1375c56d97ae50ba15f9c79123
Christopher McCormack <mccormack.christopher@gmail.com>
2015-01-02 18:18:29 -0700

    Added local XML retrieval

--- gamesdbapi.py
+++ gamesdbapi.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+import time
 import urllib
 import urllib2
 from xml.etree import ElementTree
 
-BASEURL = "http://thegamesdb.net/api/"
+API_URL = "http://thegamesdb.net/api/"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
 USER_AGENT += '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 
@@ -13,67 +15,73 @@ class Game():
 	pass
 		
 class Platform():
-
 	def __init__(self, platform_id, platform_name, platform_alias=None):
 		self.platform_id = platform_id
 		self.platform_name = platform_name
 		self.platform_alias = platform_alias
 
-class API():
+def call_api(base_url, query, query_args={}):
+	local_xml = query + '.xml'
+
+	# Check if cached version exists that was retrieved within the last hour
+	if os.path.isfile(local_xml) and (time.time() - os.path.getmtime(local_xml)) < 3600:
+		with open(local_xml) as in:
+			return in
+
+	url_values = urllib.urlencode(query_args)
+	call_url = base_url + query + '.php?' + url_values
+	print call_url
+	request = urllib2.Request(call_url)
+	request.add_unredirected_header('User-Agent', USER_AGENT)
+	query_response = urllib2.urlopen(request).read()
+
+	# Cache a copy of the XML response locally
+	with open(local_xml, 'w') as output:
+		output.write(query_response + '\n')
+
+	return query_response
+
+def getGamesList(name, platform_name=None, genre=None):
+	query = 'GetGamesList'
+	query_args = {'name':name}
+	if platform:
+		query_args['platform'] = platform_name
+	if genre:
+		query_args['genre'] = genre
+
+	response = self.call_api(API_URL, query, query_args)
+	return response
+
+
+def getPlatformsList():
+	query = 'GetPlatformsList'
+
+	response = call_api(API_URL, query)
 	
+	return parsePlatformsList(response)
+	
+def parsePlatformsList(response):
+	root = ElementTree.fromstring(response)
+	platforms = {}
+	for element in root.findall('./Platforms/Platform'):
+		platform_id = element.find('id').text
+		platform_name = element.find('name')
+		if platform_name is not None:
+			platform_name = platform_name.text
+		platform_alias = element.find('alias')
+		if platform_alias is not None:
+			platform_alias = platform_alias.text
 
-	@staticmethod
-	def call_api(base_url, query, query_args={}):
-		url_values = urllib.urlencode(query_args)
-		call_url = base_url + query + '.php?' + url_values
-		print call_url
-		request = urllib2.Request(call_url)
-		request.add_unredirected_header('User-Agent', USER_AGENT)
-		query_response = urllib2.urlopen(request).read()
+		platforms[platform_id] = platform_name, platform_alias
 
+	return platforms
 
-		# Cache a copy of the XML response locally
-		with open(query+'.xml', 'w') as output:
-			output.write(query_response + '\n')
+def getPlatformGames(platform_id):
+	query = 'GetPlatformGames'
+	query_args = {'platform':platform_id}
 
-		return query_response
-
-	def getGamesList(self, name, platform_name=None, genre=None):
-		query = 'GetGamesList'
-		query_args = {'name':name}
-		if platform:
-			query_args['platform'] = platform_name
-		if genre:
-			query_args['genre'] = genre
-
-		response = self.call_api(BASEURL, query, query_args)
-		return response
-
-
-	def getPlatformsList(self):
-		query = 'GetPlatformsList'
-
-		response = self.call_api(BASEURL, query)
-		
-		return self.parsePlatformsList(response)
-		
-	def parsePlatformsList(self, response):
-		root = ElementTree.fromstring(response)
-		platforms = {}
-		for element in root.findall('./Platforms/Platform'):
-			platform_id = element.find('id').text
-			platform_name = element.find('name').text
-			platform_alias = element.find('alias').text if element.find('alias') else None
-			platforms[platform_id] = platform_name, platform_alias
-
-		return platforms
-
-	def getPlatformGames(self, platform_id):
-		query = 'GetPlatformGames'
-		query_args = {'platform':platform_id}
-
-		response = self.call_api(BASEURL, query, query_args)
-		return response
+	response = call_api(API_URL, query, query_args)
+	return response
 
 
 	
@@ -82,7 +90,7 @@ if __name__ == "__main__":
 	with open('output.txt', 'w') as out:
 		#out.write(API().getGamesList('Super', platform = 'Super Nintendo (SNES)'))
 		#out.write(API().getPlatformGames('0'))
-		platforms = API().getPlatformsList()
-		print platforms
+		platforms = getPlatformsList()
+		#print platforms
 		for platform in platforms:
 			print platforms[platform]
