cb5c7aead52eaf570b24ef231e348ada72fa4668
Christopher McCormack <mccormack.christopher@gmail.com>
2015-01-04 20:58:42 -0700

    Lots of updates and fixes

--- gamesdbapi.py
+++ gamesdbapi.py
@@ -3,7 +3,8 @@
 
 """
 """
-
+import os
+import time
 import urllib
 import urllib2
 from xml.etree import ElementTree
@@ -14,7 +15,7 @@ USER_AGENT += '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 
 
 class Game(object):
-    """ test
+    """ To be completed
     """
 
     def __init__(self):
@@ -25,52 +26,117 @@ class Game(object):
 
 
 class Platform(object):
-    """ test
+    """ To be completed
     """
 
-    def __init__(self, platform_id, platform_name, platform_alias=None):
+    def __init__(self, platform_id, platform_name, platform_values=None):
+
+        self.p_values = platform_values
         self.p_id = platform_id
         self.p_name = platform_name
-        self.p_alias = platform_alias
+        if platform_values and platform_values['alias']:
+            self.p_alias = platform_values['alias']
+
+        if self.p_values:
+            self.p_alias = self.p_values['alias']
+            self.p_platform = self.p_values['Platform']
+            self.p_overview = self.p_values['overview']
+            self.p_developer = self.p_values['developer']
+            self.p_manufacturer = self.p_values['manufacturer']
+            self.p_cpu = self.p_values['cpu']
+            self.p_memory = self.p_values['memory']
+            self.p_graphics = self.p_values['graphics']
+            self.p_sound = self.p_values['sound']
+            self.p_display = self.p_values['display']
+            self.p_media = self.p_values['media']
+            self.p_maxcontrollers = self.p_values['maxcontrollers']
+            self.p_youtube = self.p_values['Youtube']
+            self.p_rating = self.p_values['Rating']
+            self.p_images = self.p_values['images']
 
     def __str__(self):
-        return "id:{0};name:{1};alias:{2}".format(
-            self.p_id, self.p_name, self.p_alias)
+        return '\n'.join(
+            [k+':'+self.p_values[k]for k in self.p_values if self.p_values[k]]
+        )
+
+
+class Image(object):
+    """ Image object to hold URL and dimension information for platform
+        and game images
+    """
+
+    def __init__(self, img_type, img_url, img_values=None):
+        
+        self.type = img_type
+        self.url = img_url
+
+        if img_values:
+            self.side = ''
+            self.width = ''
+            self.height = ''
+            self.thumb = ''
+
+
 
 
 def call_api(query, query_args=None):
-    """
+    """ To be completed
     """
 
     call_url = API_URL + query + '.php?'
-    call_url += urllib.urlencode(query_args) if query_args else ''
+    if query_args:
+        call_url += urllib.urlencode(query_args)
     print call_url
+    query_comment_xml = '<!--' + call_url + '-->'
+
+    # Check if cached xml exists, retrieved within the last hour
+    local_xml = query + '.xml'
+    if os.path.isfile(local_xml):
+        with open(local_xml) as input_xml:
+            if input_xml.readline().strip() == query_comment_xml:
+                time_diff = time.time() - os.path.getmtime(local_xml)
+                if time_diff < 3600:
+                    print "Found cached copy of {},".format(query),
+                    print "skipping API call."
+                    print "Last call {} minutes {} seconds ago.".format(
+                        int(time_diff/60), int(time_diff % 60))
+                    return input_xml.read()
+    # End cache check snippet -----------------------
+
     request = urllib2.Request(call_url)
     request.add_unredirected_header('User-Agent', USER_AGENT)
     query_response = urllib2.urlopen(request).read()
 
-    return query_response
+    # Cache a copy of the XML response locally
+    with open(local_xml, 'w') as output:
+        output.write(query_comment_xml + '\n')
+        output.write(query_response + '\n')
+        return query_response
+    # End cache write snippet -----------------------
 
 
 def get_platforms_list():
-    """
+    """ To be completed
     """
 
     query = 'GetPlatformsList'
 
     response = call_api(query)
     root = ElementTree.fromstring(response)
-    
+
     platforms = []
-    platform = {}
+    p_values = {
+        'id': None,
+        'name': None,
+        'alias': None
+    }
     for element in root.findall('./Platforms/Platform'):
-        platform['alias'] = ''
 
         for elem in element:
-            platform[elem.tag] = elem.text
+            p_values[elem.tag] = elem.text
 
         platforms.append(
-            Platform(platform['id'], platform['name'], platform['alias']))
+            Platform(p_values['id'], p_values['name'], p_values['alias']))
 
     return platforms
 
@@ -91,18 +157,57 @@ def get_platform(platform_id):
 
     response = call_api(query, query_args)
     root = ElementTree.fromstring(response)
-    platform = {}
-    for element in root.findall('./Platforms/Platform'):
-        platform['alias'] = ''
+
+    p_values = {
+        'id': '',
+        'Platform': '',
+        'alias': '',
+        'overview': '',
+        'developer': '',
+        'manufacturer': '',
+        'cpu': '',
+        'memory': '',
+        'graphics': '',
+        'sound': '',
+        'display': '',
+        'media': '',
+        'maxcontrollers': '',
+        'Youtube': '',
+        'Rating': '',
+        'images': ''
+    }
+
+    # Gather platform attributes from API response
+    for element in root.findall('Platform'):
+        for elem in element:
+            p_values[elem.tag] = elem.text
+
+    # Gather platform image details from API response
+    base_img_url = root.find('baseImgUrl').text
+    images = []
+
+    for element in root.findall('.//fanart'):
+        img_attribs = {
+            'side': '',
+            'width': '',
+            'height': '',
+        }
+        img = ''
+        img_type = ''
+        img_side = ''
+        img_width = ''
+        img_height = ''
+        img_thumb = ''
 
         for elem in element:
-            platform[elem.tag] = elem.text
+            # images.append(Image(element.tag, ))
+            print element.tag, elem.tag, elem.text, elem.attrib
 
-    return response
+    return Platform(p_values['id'], p_values['Platform'], p_values)
 
 
 def get_platform_games(platform_id):
-    """
+    """ To be completed
     """
 
     query = 'GetPlatformGames'
@@ -113,7 +218,7 @@ def get_platform_games(platform_id):
 
 
 def get_games_list(name, platform_name=None, genre=None):
-    """
+    """ To be completed
     """
 
     query = 'GetGamesList'
@@ -128,7 +233,7 @@ def get_games_list(name, platform_name=None, genre=None):
 
 
 def get_game(name=None, platform=None, game_id=None):
-    """
+    """ To be completed
     """
 
     query = 'GetGame'
@@ -139,5 +244,5 @@ def get_game(name=None, platform=None, game_id=None):
 
 
 if __name__ == "__main__":
-    print get_platform(1)
+    get_platform('15')
         
